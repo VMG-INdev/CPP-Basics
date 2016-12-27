7ea0a5ac0d8c0e12aaa576a9051d3c48c9f9952b
Vedant Patadia <v.mr.gamer@gmail.com>
2016-12-28 01:02:37 +0530

    Create VMrGamerHelloWorld.cpp

---
+++ HelloWorld Programs/VMrGamerHelloWorld.cpp
@@ -0,0 +1,14 @@
+//This Code Compiler is based upon Visual C++
+//Basic Win32 Console Application
+
+//By V.Mr.Gamer
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main(void){
+  cout << "Hello, World!";
+  return 0;
+}
