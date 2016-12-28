bf32d0d70cda7d2e17a609dacd55055708fb7706
nishant150498 <pnishant14@yahoo.com>
2016-12-28 19:16:13 +0530

    Update nishantcalc.cpp

--- BasicCalculator/nishantcalc.cpp
+++ BasicCalculator/nishantcalc.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 using namesapce std;
 int main();
-int "Enter your choice",'a','b';
+int Enteryourchoice,a,b;
 {
-  cout<<"Choose any one from following options:"\n;"\n\t1)Addition";"\n\t2)Subtraction";"\n\t3)Multiplication";"\n\t4)Division"<<endl;
+  cout<<"Choose any one from following options:\n\t1)Addition\n\t2)Subtraction\n\t3)Multiplication\n\t4)Division"<<endl<<"Enter your choice:";
   cin>>Enter your choice;
   cout<<"Enter 2 numbers"<<endl;
   cin>>a>>b;
-if(Enter your choice=1){
-  cout<<a+b;
+if(Enter your choice==1){
+  cout<<"Addition"<<a+b;
 }
-else(Enter your choice=2){
-  cout<<a-b;
+else if(Enter your choice==2){
+  cout<<"Subtraction"<<a-b;
 }
-else(Enter your choice=3){
-  cout<<a*b;
+else if(Enter your choice==3){
+  cout<<"Multiplication"<<a*b;
 }
-else(Enter your choice=4{
-  cout<<a/b;
+else if(Enter your choice==4){
+  cout<<"Division"<<a/b;
 }
 else cout<<"You have entered a wrong choice";
 return 0;
