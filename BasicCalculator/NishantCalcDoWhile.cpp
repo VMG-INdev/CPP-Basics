0bbfd74310e49ca9924e9a643d8572d0903e0e65
Vedant Patadia <v.mr.gamer@gmail.com>
2016-12-29 15:42:58 +0530

    Update NishantCalcDoWhile.cpp

--- BasicCalculator/NishantCalcDoWhile.cpp
+++ BasicCalculator/NishantCalcDoWhile.cpp
@@ -1,26 +1,25 @@
 #include<iostream>
 using namesapce std;
-int main();{
-do{
-int Enteryourchoice,a,b;
-  cout<<"Choose any one from following options:\n\t1)Addition\n\t2)Subtraction\n\t3)Multiplication\n\t4)Division"<<endl<<"Enter your choice:";
-  cin>>Enter your choice;
-  cout<<"Enter 2 numbers"<<endl;
-  cin>>a>>b;
-if(Enter your choice==1){
-  cout<<"Addition"<<a+b;
-}
-else if(Enter your choice==2){
-  cout<<"Subtraction"<<a-b;
-}
-else if(Enter your choice==3){
-  cout<<"Multiplication"<<a*b;
-}
-else if(Enter your choice==4){
-  cout<<"Division"<<a/b;
-}
-else cout<<"You have entered a wrong choice";
-}
-while(You have entered a wrong choice);
-return 0;
+int main(){
+  do{
+    int Enteryourchoice,a,b;
+    cout<<"Choose any one from following options:\n\t1)Addition\n\t2)Subtraction\n\t3)Multiplication\n\t4)Division"<<endl<<"Enter your choice:";
+    cin>>Enter your choice;
+    cout<<"Enter 2 numbers"<<endl;
+    cin>>a>>b;
+    if(Enter your choice==1){
+      cout<<"Addition"<<a+b;
+    }
+    else if(Enter your choice==2){
+      cout<<"Subtraction"<<a-b;
+    }
+    else if(Enter your choice==3){
+      cout<<"Multiplication"<<a*b;
+    }
+    else if(Enter your choice==4){
+      cout<<"Division"<<a/b;
+    }
+    else cout<<"You have entered a wrong choice";
+  }while(/*Replace it with a proper condition*/);//It would be better if I wrote the code myself
+  return 0;
 }
