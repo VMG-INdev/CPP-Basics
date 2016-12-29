7915912bcbd9e77d36556f58a458cc7f87c53fc8
Vedant Patadia <v.mr.gamer@gmail.com>
2016-12-29 09:54:10 +0530

    Add files via upload

---
+++ ForPatterns.cpp
@@ -0,0 +1,64 @@
+// ForPatterns.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main(){
+	int a;
+	cout << "What is the Square side: ";
+	cin >> a;
+
+	for (int i = 0;i < a;i++) {
+		for (int j = 0; j < a; j++) {
+			cout << "*";
+		}
+		cout << endl;//cout << '\n';
+	}
+	cout << endl;
+	for (int i = 0;i < a;i++) {
+		for (int j = 0;j <= i;j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0;i < a;i++) {
+		for (int j = 0;j < a - i;j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0;i < a;i++) {
+		for (int j = 0;j < a - i - 1;j++) {
+			cout << " ";
+		}
+		for (int j = 0;j <= i;j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0;i < a;i++) {
+		for (int j = 0;j < i;j++) {
+			cout << " ";
+		}
+		for (int j = 0;j < a - i;j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	/*
+
+	Pyramid
+	Inverted Pyramid
+	Rectangle
+
+	*/
+    return 0;
+}
+
