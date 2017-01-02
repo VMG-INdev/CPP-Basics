71a056380d8aa2a5cffaa7098c6a923ebad8d905
nishant150498 <pnishant14@yahoo.com>
2017-01-02 19:33:56 +0530

    Create nishant_pattern_3.cpp

---
+++ Patterns/nishant_pattern_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,r;
+    cout<<"Enter the number of rows: ";
+    cin>>r;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+           cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
