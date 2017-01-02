6b17bdcad9157011707a45f024455a26928343d8
nishant150498 <pnishant14@yahoo.com>
2017-01-02 19:31:06 +0530

    Create nishant_pattern_1.cpp

---
+++ Patterns/nishant_pattern_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r,i,j;
+     cout<<"What is the side of square:";
+     cin>>r;
+      for(i=0;i<r;i++)
+        {
+            for(j=0;j<r;j++)
+                 cout<<"*";
+                 cout<<endl;
+      }
+      return 0;
+
+}
