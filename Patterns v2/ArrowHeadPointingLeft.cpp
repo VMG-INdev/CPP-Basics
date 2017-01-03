63fd048e32764e6da0dd3f2b2b271805c6402d12
nishant150498 <pnishant14@yahoo.com>
2017-01-03 19:51:22 +0530

    Create ArrowHeadPointingLeft.cpp

---
+++ Patterns v2/ArrowHeadPointingLeft.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r,i,j;
+    cout<<"Enter the number of rows: ";
+    cin>>r;
+
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<=i;j++)
+            cout<<"*";
+            cout<<endl;
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<r-i;j++)
+            cout<<"*";
+            cout<<endl;
+    }
+    return 0;
+}
